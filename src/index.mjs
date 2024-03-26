c4fb4744d826a39a25f3c76ec71665ef5badade1
ehandresen <eirik.horgen.andresen@gmail.com>
2024-03-27 00:45:37 +0100

    add endpoint

--- src/index.mjs
+++ src/index.mjs
@@ -8,6 +8,14 @@ app.get('/', (request, response) => {
   response.send({ msg: 'Hello' });
 });
 
+app.get('/api/users', (request, response) => {
+  response.send([
+    { id: 1, user: 'james', password: 'james123' },
+    { id: 2, user: 'jane', password: 'jane123' },
+    { id: 3, user: 'danny', password: 'danny123' },
+  ]);
+});
+
 app.listen(PORT, () => {
   console.log(`Running on port: ${PORT}`);
 });
