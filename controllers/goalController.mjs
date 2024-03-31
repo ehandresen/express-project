a12db19e3144eb2a7ec3de9f6c74c0b4ac2424cc
ehandresen <eirik.horgen.andresen@gmail.com>
2024-03-31 16:30:54 +0200

    added express-async-handler

--- controllers/goalController.mjs
+++ controllers/goalController.mjs
@@ -1,28 +1,30 @@
+import asyncHandler from 'express-async-handler';
+
 // @desc Get goals
 // @route GET /api/goals
-function getGoals(request, response) {
+const getGoals = asyncHandler(async (request, response) => {
   response.status(200).json({ message: 'Get goals' });
-}
+});
 
 // @desc Set goals
 // @route POST /api/goals
-function setGoal(request, response) {
+const setGoal = asyncHandler(async (request, response) => {
   if (!request.body.text) {
     response.status(400);
     throw new Error('Please add a text field');
   }
-}
+});
 
 // @desc Update goal
 // @route PUT /api/goals/:id
-function updateGoal(request, response) {
+const updateGoal = asyncHandler(async (request, response) => {
   response.status(200).json({ message: `Update goal ${request.params.id}` });
-}
+});
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
-function deleteGoal(request, response) {
+const deleteGoal = asyncHandler(async (request, response) => {
   response.status(200).json({ message: `Delete goal ${request.params.id}` });
-}
+});
 
 export { getGoals, setGoal, updateGoal, deleteGoal };
--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import dotenv
+import 'dotenv/config';
 import router from '../routes/goalRoutes.mjs';
 import errorHandler from '../middleware/errorMiddleware.mjs';
 
