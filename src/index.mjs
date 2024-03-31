2a4c5d7d99c14b67618c73581829ddf3aa277df4
ehandresen <eirik.horgen.andresen@gmail.com>
2024-03-31 17:24:32 +0200

    added config folder with db.mjs that connects to mongoDB with mongoose

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,11 @@
 import express from 'express';
+import colors from 'colors';
 import 'dotenv/config';
 import router from '../routes/goalRoutes.mjs';
 import errorHandler from '../middleware/errorMiddleware.mjs';
+import connectDB from '../config/db.mjs';
+
+connectDB();
 
 const app = express();
 
